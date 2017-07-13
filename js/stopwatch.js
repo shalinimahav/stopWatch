6e0be709d233c646d99c00fa157d84bea17ac182
shalinimahav <shalini.mahav@gmail.com>
2017-07-13 16:56:45 -0500

    adding functions to start and reset using OOPs concepts

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,1 +1,77 @@
-console.log('hi');
+//a function that creates an object is basically a constructor function
+//we use it with a new operator
+//anything given to this is printed into watch
+//variables that are not pre fixed with 'this' are local to just that function
+
+function stopWatch(){
+
+	var time=0;
+	var interval;
+	var offSet;   					//To calculate how much time is passed.
+
+	function update() {				//Private Functions
+		time += delta();
+		var formattedTime = timeFormatter(time);
+	}
+
+	function delta() {
+
+		var now = Date.now();
+		var timePassed = now - offSet;
+		offset = now;
+		return timePassed;
+	}
+
+	function timeFormatter(timeInMilliseconds) {
+		var time = new Date(timeInMilliseconds);
+		var minutes = time.getMinutes().toString();
+		var seconds = time.getSeconds().toString();
+		var milliseconds = time.getMilliseconds().toString();
+		if(minutes.length < 2){
+			minutes = '0' + minutes;
+		}
+
+		if(seconds.length < 2){
+			seconds = '0' + seconds;
+		}
+
+		while(milliseconds.length < 2){
+			milliseconds = '0' + milliseconds;
+		}
+
+		return minutes + ":" + seconds + ":" + milliseconds;
+	}
+
+	this.isOn = false;  	        					//to check if stop watch is currently running  
+														//(initializing it to false)
+
+
+	this.start = function(){
+		if(!this.isOn){
+			interval = setInterval(update, 10);
+			offSet = Date.now();
+			this.isOn = true;  	        	
+
+		}
+	};
+
+
+	this.stop = function(){
+		clearInterval(interval);
+		interval = null;
+		this.isOn = false;
+	};
+
+
+
+	this.reset = function(){
+		time = 0;
+	};
+
+}
+var watch = new stopWatch(); 
+
+
+								//when we use 'new' keyword with a function,
+								// that function implicitly creates an empty object & returns 'this'.
+
